bc34be3b1e1aefb22c36030d21518e20b5e9905f
Kiran <colkiran@gmail.com>
2024-05-17 12:04:17 +0530

    Commit

---
+++ Day01/O06Lambdas01.py
@@ -0,0 +1,32 @@
+
+def add(x, y):
+    return x + y
+
+print(add(10, 20))
+
+a = add
+res = a(100, 200)
+print(f"res :{res}")
+
+print("-" * 60)
+
+x = lambda a, b: a + b
+print(x(10, 20))
+
+print("-" * 60)
+# map
+
+l1 = list(range(1, 11))
+print(f"l1 :{l1}")
+
+res = list(map(lambda x: x ** 2, l1))
+print(f"res :{res}")
+
+# conversions - currency (rs - dollars, dollar - pounds)
+#             - Weight (pounds - kgs)
+
+months = ['nov', 'jul', 'dec', 'aug', 'sep', 'mar', 'may', 'oct', 'feb', 'jun', 'jan', 'apr']
+
+print(len(months))
+
+# sort these months according to calendar dates
